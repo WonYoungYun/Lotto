2c7d8e559525b65668445e5291719edfc5d0764b
WonYoungYun <asmar20288@naver.com>
2018-07-17 15:19:45 +0900

    1.0.1

--- src/main/java/com/wonyoung/lotto/controllers/LottoBoardController.java
+++ src/main/java/com/wonyoung/lotto/controllers/LottoBoardController.java
@@ -4,12 +4,15 @@ package com.wonyoung.lotto.controllers;
 import com.wonyoung.lotto.models.LottoBoard;
 import com.wonyoung.lotto.repositories.LottoBoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 public class LottoBoardController {
 
     @Autowired
@@ -17,9 +20,9 @@ public class LottoBoardController {
 
 
     @GetMapping("/")
-    public List<LottoBoard> LottoIndex(){
-        List<LottoBoard> boardList = lottoRepo.findAllByOrderByRegdate();
-        return boardList;
+    public String LottoIndex(Model model){
+        model.addAttribute("result", lottoRepo.findAllByOrderByRegdate());
+        return "index";
     }
 
 
